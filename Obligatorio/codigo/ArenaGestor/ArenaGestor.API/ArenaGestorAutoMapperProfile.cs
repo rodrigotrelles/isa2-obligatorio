54fe26a896e94c3680cbc1f8123ee2d9ff646dab
federicop <federicop@dlya.com.uy>
2023-04-17 22:59:53 -0300

    style: Formatted files

--- Obligatorio/codigo/ArenaGestor/ArenaGestor.API/ArenaGestorAutoMapperProfile.cs
+++ Obligatorio/codigo/ArenaGestor/ArenaGestor.API/ArenaGestorAutoMapperProfile.cs
@@ -88,7 +88,7 @@ namespace ArenaGestor.API
             CreateMap<List<UserRoleDto>, IEnumerable<UserRole>>();
             CreateMap<UserRole, UserRoleDto>().ForMember(x => x.Name, y => y.MapFrom(y => y.Role.Name));
             CreateMap<List<UserRole>, IEnumerable<UserRoleDto>>();
-          
+
 
             CreateMap<ConcertGetConcertsDto, ConcertFilter>();
             CreateMap<ConcertGetDateRangeConcertsDto, DateRange>();
